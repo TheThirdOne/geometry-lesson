26940c5455386f7233939a60c3f81c07de65944d
Benjamin Landers <benjaminrlanders@gmail.com>
2015-04-05 15:15:37 -0700

    added more shapes

--- main.js
+++ main.js
@@ -6,11 +6,16 @@ canvas.width  = window.innerWidth; //canvas should take up the entire screen
 canvas.height = window.innerHeight;
 canvas.width  -= canvas.width%size+size-1; //make the edges line up + not need scrollbars
 canvas.height -= canvas.height%size+size-1;
-var templates = [new Shape([1,2,3],[3,1,3]),
-                 new Shape([4,5,7,6],[3,1,1,3]),
-                 new Shape([8,9,11,12],[3,1,1,3])];
+var templates = [new Shape([1,1,3],[3,1,3]),
+                 new Shape([4,5,6],[3,1,3]),
+                 new Shape([7,10,9],[3,1,3]),
+                 new Shape([11,11,13,13],[3,1,1,3]),
+                 new Shape([14,15,17,16],[3,1,1,3]),
+                 new Shape([18,19,21,22],[3,1,1,3]),
+                 new Shape([23,24,25,27],[3,1,1,3]),
+                 new Shape([28,29,31,32,31,29],[3,1,1,3,5,5])];
 
-var shapes = [new Shape([1,2,3],[3,1,3]),new Shape([1,2,4,5],[6,4,4,6])];
+var shapes = [];
 shapes.selected = -1;
 
 
